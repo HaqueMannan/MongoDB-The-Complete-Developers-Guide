ec96ebce55d909830c050f325f99c9dca4bb19bb
Haque <34745319+HaqueMannan@users.noreply.github.com>
2019-05-05 17:30:45 +0100

    Section 17 - From Shell to Driver - 17.4. Connecting Node.js and the MongoDB Cluster.

--- Driver to Shell Project/ShellToDriverAppCode/backend/app.js
+++ Driver to Shell Project/ShellToDriverAppCode/backend/app.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoClient = require('mongodb').MongoClient;
 
 const productRoutes = require('./routes/products');
 const authRoutes = require('./routes/auth');
@@ -24,5 +25,14 @@ app.use((req, res, next) => {
 
 app.use('/products', productRoutes);
 app.use('/', authRoutes);
+
+mongoClient.connect('mongodb+srv://testUser:bwXIYOay2PyOdLey@fromshelltodrivertutorial-gtajb.mongodb.net/test?retryWrites=true')
+   .then( client => {
+      console.log('Connected!');
+      client.close();
+   })
+   .catch( err => { 
+      console.log(err);
+   });
 
 app.listen(3100);
