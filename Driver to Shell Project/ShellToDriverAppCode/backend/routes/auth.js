d42673543a4a4df165e2dac8632eabceb9340160
Haque <34745319+HaqueMannan@users.noreply.github.com>
2019-05-11 14:40:05 +0100

    Section 17 - From Shell to Driver - 17.13. Add User Sign Up.

--- Driver to Shell Project/ShellToDriverAppCode/backend/routes/auth.js
+++ Driver to Shell Project/ShellToDriverAppCode/backend/routes/auth.js
@@ -1,8 +1,8 @@
 const Router = require('express').Router;
+const router = Router();
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
-
-const router = Router();
+const db = require('../db');
 
 const createToken = () => {
    return jwt.sign({}, 'secret', { expiresIn: '1h' });
@@ -15,9 +15,7 @@ router.post('/login', (req, res, next) => {
    // If yes, create token and return it to client
    const token = createToken();
    // res.status(200).json({ token: token, user: { email: 'dummy@dummy.com' } });
-   res
-   .status(401)
-   .json({ message: 'Authentication failed, invalid username or password.' });
+   res.status(401).json({ message: 'Authentication failed, invalid username or password.' });
 });
 
 router.post('/signup', (req, res, next) => {
@@ -28,17 +26,26 @@ router.post('/signup', (req, res, next) => {
    .hash(pw, 12)
    .then(hashedPW => {
       // Store hashedPW in database
-      console.log(hashedPW);
-      const token = createToken();
-      res
-      .status(201)
-      .json({ token: token, user: { email: 'dummy@dummy.com' } });
+      db.getDb()
+         .db()
+         .collection('users').insertOne({
+           email: email,
+           password: hashedPW
+         })
+            .then(result => {
+            console.log(result);
+            const token = createToken();
+            res.status(201).json({ token: token, user: { email: email } });
+            })
+            .catch(err => {
+            console.log(err);
+            res.status(500).json({ message: 'Creating the user failed.' });
+            });
    })
    .catch(err => {
       console.log(err);
       res.status(500).json({ message: 'Creating the user failed.' });
    });
-   // Add user to database
 });
 
 module.exports = router;
